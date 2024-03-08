bdf2135688e10cbd2fa4dcf94763d2089f1e758d
GHMazadun <162692073+GHMazadun@users.noreply.github.com>
2024-03-08 18:57:49 +0300

    Add files via upload

---
+++ max_length.py
@@ -0,0 +1,15 @@
+string_list = ['cat', 'horse', 'dog', 'crocodile', 'elephant', 'snake']
+
+def max_length(string_list):
+    max_word_length = 0  # Задаем базовое значение для длины строки
+    
+    for index, word in enumerate(string_list):
+        word_length = len(word)
+        if word_length > max_word_length:
+            max_word_length = word_length  # Обновляем базовое значение длины строки по мере прохождения списка
+    
+    return max_word_length
+
+# Выводим результат
+max_word_length = max_length(string_list)
+print("Длина самого длинного слова из таблицы:", max_word_length)
